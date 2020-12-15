a72d8971588bc57b698022f27e656e90da31e315
Alan Kelly <alankelly@Alans-MacBook.home>
2020-12-15 08:02:57 +0000

    wire up card click

--- Data/Card.cs
+++ Data/Card.cs
@@ -4,8 +4,14 @@ namespace gameserver.Data
 {
     public class Card
     {
+        public string ID { get; set; }
         public string Name { get; set; }
         public int Value { get; set; }
         public string Colour { get; set; }
+
+        public Card()
+        {
+            ID = Guid.NewGuid().ToString("N");
+        }
     }
 }
